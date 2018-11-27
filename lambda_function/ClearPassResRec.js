3f9469de853dbe7ab6a78b1ade91016b7274338b
JClayF <42550550+JClayF@users.noreply.github.com>
2018-11-26 19:56:51 -0500

    Create ClearPassResRec.js

---
+++ lambda_function/ClearPassResRec.js
@@ -0,0 +1,57 @@
+var mysql = require('./node_modules/mysql');
+var config = require('./config.json');
+var validator = require('./validation.js');
+
+function formatErrorResponse(code, errs) {
+	return JSON.stringify({ 
+		error  : code,
+		errors : errs
+	});
+}
+
+exports.handler = (event, context, callback) => {
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	//validate input
+	var errors = new Array();
+	
+	 // Validate the user input
+	validator.validatePassRes(event.passwordresetid, errors);
+	
+	if(errors.length > 0) {
+		// This should be a "Bad Request" error
+		callback(formatErrorResponse('BAD_REQUEST', errors));
+	} else {
+	
+		//getConnection equivalent
+		var conn = mysql.createConnection({
+			host 	: config.dbhost,
+			user 	: config.dbuser,
+			password : config.dbpassword,
+			database : config.dbname
+		});
+		
+	
+		//attempts to connect to the database
+		conn.connect(function(err) {
+		  	
+			if (err)  {
+				// This should be a "Internal Server Error" error
+				callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+			};
+			console.log("Connected!");
+			var sql = "DELETE FROM passwordreset WHERE passwordresetid = ? OR expires < NOW()";
+			
+			conn.query(sql, [event.passwordresetid], function (err, result) {
+			  	if (err) {
+					// This should be a "Internal Server Error" error
+					callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+			  	} else {
+			    	console.log("Record deleted");
+			    	
+			    	callback(null,"Record deleted");
+				} //session deleted
+		  	}); //query delete sessopn
+		}); //connect database
+	} //no validation errors
+} //handle
